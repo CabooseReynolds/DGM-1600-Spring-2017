b0d42c5c9badc7d83c2f9acffb69da1662879dc8
Unknown <caboosereynolds@gmail.com>
2017-02-02 18:18:53 -0700

    Created 3 classes

---
+++ Classes/MovementControls.cs
@@ -0,0 +1,11 @@
+public class MovementControls : MonoBehavior
+{
+    public float speed;
+    public float turnSpeed;
+    
+    public class speed {
+        public int machspeed = 1;
+        public int lightspeed = 0;
+        public int topspeed = 1400;
+    }
+}
