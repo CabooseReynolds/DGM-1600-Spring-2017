d625836f56b4ab8ddbc7036fa4c298122c1316c3
Unknown <caboosereynolds@gmail.com>
2017-02-23 17:29:12 -0700

    switch statement homework

--- Codes/Switch.cs
+++ Codes/Switch.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 using System.Collections;
 
-public class Switch : MonoBehaviour {
+public class Switch : MonoBehaviour  {
 
-    //Use this for initilization
+    /*
+Streamlined conditional to compare a single variable to a series of constants
+
+make decisions based on enumerations
+
+Characters conversation options based on their intelligence
+You could use if else statemtents but they become combersome, if you start checking for more than 2 conditions, you want to use switch statements.
+
+you have to start the command with "Switch" then followed by the integer.
+
+then define a series of cases. include as many as needed
+
+then create a jump statement by using "break" this will stop the code from going through the rest of the cases.*/
+
     void Start () {
         string light = "Green";
 
