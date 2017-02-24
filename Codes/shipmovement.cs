6de29f87defebc58c4474dd7e98f6b1bf9937c94
Unknown <caboosereynolds@gmail.com>
2017-02-23 18:47:48 -0700

    New Stages

---
+++ Codes/shipmovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class shipmovement : MonoBehaviour {
+
+    public float moveSpeed;
+
+    void Start ()
+    {
+        moveSpeed = 1f;
+    }
+
+    void update ()
+    { transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltatime);
+    }
+}
