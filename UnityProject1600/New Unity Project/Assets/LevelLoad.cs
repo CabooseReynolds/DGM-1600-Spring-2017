c99f9b24cd785c03654f5df6ba8e21ba34c6f4b7
Unknown <caboosereynolds@gmail.com>
2017-03-09 19:17:35 -0700

    unity project update

---
+++ UnityProject1600/New Unity Project/Assets/LevelLoad.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour {
+
+    public void LoadLevel(string level){
+
+        SceneManager.LoadScene(level);
+    }
+}
